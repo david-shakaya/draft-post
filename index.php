e27879f3cc711de925734ee87de33114b2e11c33
Давид Шакая <66774063+david-shakaya@users.noreply.github.com>
2021-07-23 19:02:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -102,5 +102,58 @@ mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251");
     
 echo "<p style='color:green;'>Уважаемый(ая) <b style='color:red;'>$name1</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b style='color:red;'> $email1</b>.</p>";
 }
+	
+	
+	
+	
+	//От Анны БЛОК https://www.youtube.com/watch?v=Yn1NafVnfpA
+	
+	
+	<?php
+if (isset ($_POST['contactFF'])) {
+  $to = "example@gmail.com";
+  $from = "support@tpverstak.ru";
+  $subject = "Заполнена контактная форма на сайте ".$_SERVER['HTTP_REFERER'];
+  $message = "Имя пользователя: ".$_POST['nameFF']."\nEmail пользователя ".$_POST['contactFF']."\nТелефон пользователя ".$_POST['telFF']."\nСообщение: ".$_POST['projectFF']."\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
+ 
+  $boundary = md5(date('r', time()));
+  $filesize = '';
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "From: " . $from . "\r\n";
+  $headers .= "Reply-To: " . $from . "\r\n";
+  $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+  $message="
+Content-Type: multipart/mixed; boundary=\"$boundary\"
+ 
+--$boundary
+Content-Type: text/plain; charset=\"utf-8\"
+Content-Transfer-Encoding: 7bit
+ 
+$message";
+     if(is_uploaded_file($_FILES['fileFF']['tmp_name'])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['fileFF']['tmp_name'])));
+         $filename = $_FILES['fileFF']['name'];
+         $filetype = $_FILES['fileFF']['type'];
+         $filesize = $_FILES['fileFF']['size'];
+         $message.="
+ 
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+ 
+$attachment";
+     }
+   $message.="
+--$boundary--";
+ 
+  if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
+    mail($to, $subject, $message, $headers);
+    echo $_POST['nameFF'].', Ваше сообщение отправлено, спасибо!';
+  } else {
+    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
+  }
+}
+?>
 
 ?>
